5f9aa47e6b4a7c98358cf21a7e47cb78d5bfc7f7
Ryan Drapeau <drapeau@cs.washington.edu>
2014-08-24 15:43:57 -0700

    Changed to multi class perceptron and added MNIST data set

--- perceptron.py
+++ perceptron.py
@@ -3,16 +3,18 @@ This is an implentation of a Perceptron (http://en.wikipedia.org/wiki/Perceptron
 
 @author Ryan Drapeau
 """
-from numpy import dot, array
+from numpy import alterdot, array, dot
 
 class Perceptron(object):
 
-    def __init__(self):
-        self.weights = None
+    def __init__(self, number_of_classes, dimension):
+        self.weights = []
+        for i in xrange(number_of_classes):
+            self.weights.append([0] * (dimension + 1))
+
         self.learning_rate = 0.1
         self.max_iterations = 100
-        self.dimension = None
-        self.threshold = 0.5
+        self.dimension = dimension
 
 
     def set_learning_rate(self, learning_rate):
@@ -34,7 +36,12 @@ class Perceptron(object):
 
 
     def set_weights(self, weights):
-        self.dimension = len(weights) - 1
+        """
+        Sets the weight matrix the algorithm will use.
+
+        weights (Array[(Array[(float)])]) - The weight matrix to use
+        """
+        self.dimension = len(weights[0]) - 1
         self.weights = weights
 
 
@@ -50,8 +57,6 @@ class Perceptron(object):
         if len(training_samples) != len(training_labels) or len(training_samples) < 1:
             raise Exception("Samples and labels must have the same number of entries")
 
-        self.dimension = len(training_samples[0])
-        self.weights = array([0] * (self.dimension + 1))
         finished = False
         count = 0
         training_samples = [array([1] + sample) for sample in training_samples]
@@ -61,51 +66,52 @@ class Perceptron(object):
             for i, sample in enumerate(training_samples):
                 finished &= self.train(sample, training_labels[i])
 
+            print 'Finished Pass', count
+
         print 'Finished Training'
-        # print self.weights
 
 
     def train(self, sample, label):
-        value = self.__step_function(dot(sample, self.weights))
+        """
+        Trains a single sample.
 
-        if self.__step_function(value) != self.__step_function(label):
-            self.__update_weights(label - value, sample)
+        sample (Array[float]) - The sample vector
+        label (int) - The label class for this sample
+        """
+        max_class = array([dot(sample, weight) for weight in self.weights]).argmax()
+
+        if max_class != label:
+            self.__update_weights(label, max_class, sample)
             return False
 
         return True
 
 
-    def __step_function(self, value):
-        """
-        Evalutes the value to a step function based on the threshold.
-
-        @return (int): class 1 if the value is above the threshold, otherwise 0
-        """
-        return 1 if value > self.threshold else 0
-
-
-    def __update_weights(self, error, sample):
+    def __update_weights(self, label, max_class, sample):
         """
         Updates the weight vector with the error and the sample.
 
-        error (float) - The error to update with
+        label (int) - The true label class for this sample
+        max_class (int) - The incorrect label class for this sample
         sample (Array[(float)]) - The sample vector
         """
-        for i, weight in enumerate(self.weights):
-            self.weights[i] = weight + self.learning_rate * error * sample[i]
+        for i in xrange(len(self.weights[max_class])):
+            self.weights[max_class][i] -= self.learning_rate * sample[i]
+
+        for i in xrange(len(self.weights[label])):
+            self.weights[label][i] += self.learning_rate * sample[i]
 
 
-    def classify(self, input_vector):
+    def classify(self, sample):
         """
-        Classifies the input_vector.
+        Classifies the sample.
 
-        input_vector (Array[(float)]) - The vector to classify
+        sample (Array[(float)]) - The vector to classify
         @throws (Exception) if the Perceptron was not trained or if the vector's dimension is incorrect
         @return (int) The classification of the input vector
         """
-        if self.weights == None or len(input_vector) != self.dimension:
+        if self.weights == None or len(sample) != self.dimension:
             raise Exception("Illegal dimension or not trained")
 
-        input_vector = [1] + input_vector
-        value = dot(input_vector, self.weights)
-        return self.__step_function(value)
+        sample = array([1] + sample)
+        return array([dot(sample, weight) for weight in self.weights]).argmax()
